6457730c4f0b9d2e5528d90ec642fad23822ff37
synch404 <mihai.topala@ati.utm.md>
2018-03-31 16:41:26 +0300

    unstaged files

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
+function Car(name, year, price) {
+    this.name = name;
+    this.year = year;
+    this.price = price;
+}
+
 const Garage = function ()  {
 
-    const cars = [];
-    
-    const addNewCar = car => {
-         cars.push(car);
-    } 
- 
-    const sortByYear =  () => {
-        cars.sort((a,b )=> {
-            return a.year - b.year;
-        })
-    }
- 
-    const displayCarsInfo =  () => {
-         console.log(cars);
-    }
- 
-    const getCheapsCars = () => {
-       return cars.filter(car => car.price < 5000);
-    }
-    
-    const iterator = () => {
-        for(var prop in cars) {
+   const cars = [];
+   
+   const addNewCar = (name, year, price) => {
+        cars.push(new Car(name, year, price));
+   } 
+
+   const sortByYear =  () => {
+       cars.sort((a,b )=> {
+           return a.year - b.year;
+       })
+   }
+
+   const displayAllCars =  () => {
+        return cars;
+   }
+
+   const getCheapsCars = () => {
+      return cars.filter(car => car.price < 5000);
+   }
+   
+   const iterator = () =>  {
+       for(var prop in cars) {
             if(cars.hasOwnProperty(prop)) {
                 console.log(cars[prop]);
             }
         }
-     }
- 
-    return {
-        addNewCar: addNewCar,
-        getCheapsCars: getCheapsCars,
-        sortCarsByYear: sortByYear,
-        dispalyAllCars: displayCarsInfo,
-        iterator: iterator
     }
-     
- };
- 
- const garage = new Garage();
- garage.addNewCar({name: 'test', year:  2017, price: 5000});
- garage.addNewCar({name: 'test', year:  1990, price: 2000});
- 
- var car = garage.getCheapsCars();
- console.log(car);
- 
- 
- 
- 
- 
- 
+
+   return {
+       addNewCar: addNewCar,
+       getCheapsCars: getCheapsCars,
+       sortCarsByYear: sortByYear,
+       dispalyAllCars: displayAllCars,
+       iterator: iterator
+   }
+    
+};
+
+function generateNewGarageModule() {
+    return new Garage();
+}
+
+var garage = Garage();
+
+garage.addNewCar('Audi', 2016, 6000);
+garage.addNewCar('Dacia', 2007, 4500);
+
+garage.iterator();
